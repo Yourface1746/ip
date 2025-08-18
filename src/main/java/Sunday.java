b3d8d6274183ebd4c9e76681ececc41944d6e253
Yourface1746 <Zshouhe@gmail.com>
2025-08-18 18:22:11 +0800

    Level 2 - Add task with list support command

--- src/main/java/Sunday.java
+++ src/main/java/Sunday.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Sunday {
     public static void main(String[] args) {
         Scanner myInput = new Scanner(System.in);
+        ArrayList<String> taskList = new ArrayList<>(100);
 
         System.out.println("--------------------------------");
         System.out.println("Hi, I am Sunday. \nYour personal chatbot. :)");
@@ -14,7 +16,14 @@ public class Sunday {
 
         while(!Objects.equals(userInput, "bye")) {
             System.out.println("--------------------------------");
-            System.out.println(userInput);
+            if (Objects.equals(userInput, "list")) {
+                for(int i = 0; i < taskList.size(); i++) {
+                    System.out.println(i + 1 + ". " + taskList.get(i));
+                }
+            } else {
+                System.out.println("added: " + userInput);
+                taskList.add(userInput);
+            }
             System.out.println("\n--------------------------------\n");
             userInput = myInput.nextLine();
         }
