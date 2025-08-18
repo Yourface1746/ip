f77685b3220adbbc8e80c81505dc0d0a65bd9842
Yourface1746 <Zshouhe@gmail.com>
2025-08-18 18:07:47 +0800

    Level-1: Added Echo, list, added bye command

--- src/main/java/Sunday.java
+++ src/main/java/Sunday.java
@@ -1,7 +1,23 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Sunday {
     public static void main(String[] args) {
-        System.out.println("Hi, I am Sunday.");
-        System.out.println("How can I help you today?.");
+        Scanner myInput = new Scanner(System.in);
+
+        System.out.println("--------------------------------");
+        System.out.println("Hi, I am Sunday. \nYour personal chatbot. :)");
+        System.out.println("How can I help you today?. (0.0)");
+        System.out.println("\n--------------------------------\n");
+
+        String userInput = myInput.nextLine();
+
+        while(!Objects.equals(userInput, "bye")) {
+            System.out.println("--------------------------------");
+            System.out.println(userInput);
+            System.out.println("\n--------------------------------\n");
+            userInput = myInput.nextLine();
+        }
 
         System.out.println("\nSee you next time! :)");
     }
