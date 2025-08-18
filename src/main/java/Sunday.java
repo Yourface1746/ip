8d5c9f878b60b8282ed670ce1792524032c7f8d1
Yourface1746 <Zshouhe@gmail.com>
2025-08-18 17:41:14 +0800

    Level-0: Add greeting

---
+++ src/main/java/Sunday.java
@@ -0,0 +1,8 @@
+public class Sunday {
+    public static void main(String[] args) {
+        System.out.println("Hi, I am Sunday.");
+        System.out.println("How can I help you today?.");
+
+        System.out.println("\nSee you next time! :)");
+    }
+}
