757ac2d3ce2d9adfd1eb3cccbe91c2c578e691d6
Yourface1746 <Zshouhe@gmail.com>
2025-08-19 10:49:51 +0800

    Level-3: Added a task class. Added mark and unmark

--- src/main/java/Sunday.java
+++ src/main/java/Sunday.java
@@ -1,3 +1,4 @@
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
@@ -5,7 +6,7 @@ import java.util.Scanner;
 public class Sunday {
     public static void main(String[] args) {
         Scanner myInput = new Scanner(System.in);
-        ArrayList<String> taskList = new ArrayList<>(100);
+        ArrayList<Task> taskList = new ArrayList<>(100);
 
         System.out.println("--------------------------------");
         System.out.println("Hi, I am Sunday. \nYour personal chatbot. :)");
@@ -13,19 +14,37 @@ public class Sunday {
         System.out.println("\n--------------------------------\n");
 
         String userInput = myInput.nextLine();
+        String[] parts = userInput.split(" ", 2);
+        for (int j = 0; j < 2; j++) {
+            System.out.println(parts[j]);
+        }
 
-        while(!Objects.equals(userInput, "bye")) {
+        while(!Objects.equals(parts[0], "bye")) {
             System.out.println("--------------------------------");
-            if (Objects.equals(userInput, "list")) {
-                for(int i = 0; i < taskList.size(); i++) {
-                    System.out.println(i + 1 + ". " + taskList.get(i));
+            if (Objects.equals(parts[0], "list")) {
+                for (int i = 0; i < taskList.size(); i++) {
+                    Task task = taskList.get(i);
+                    System.out.println(i + 1 + ". [" + task.getStatus() + "] " + task.toString());
                 }
+            } else if (Objects.equals(parts[0], "mark")) {
+                int pos = Integer.parseInt(parts[1]);
+                Task task = taskList.get(pos - 1);
+                task.markAsDone();
+                System.out.println("Got it. I have marked this task as done.");
+                System.out.println("    [" + task.getStatus() + "] " + task.toString());
+            } else if (Objects.equals(parts[0], "unmark")) {
+                int pos = Integer.parseInt(parts[1]);
+                Task task = taskList.get(pos - 1);
+                task.markAsUndone();
+                System.out.println("Got it. I have marked this task as undone.");
+                System.out.println("    [" + task.getStatus() + "] " + task.toString());
             } else {
-                System.out.println("added: " + userInput);
-                taskList.add(userInput);
+                System.out.println(" have added: " + parts[1]);
+                taskList.add(new Task(parts[1]));
             }
             System.out.println("\n--------------------------------\n");
             userInput = myInput.nextLine();
+            parts = userInput.split(" ", 2);
         }
 
         System.out.println("\nSee you next time! :)");
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String taskName;
+    private boolean completed;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.completed = false;
+    }
+
+    public String getStatus() {
+        return this.completed ? "X" : " ";
+    }
+
+    public void markAsDone() {
+        this.completed = true;
+    }
+
+    public void markAsUndone() {
+        this.completed = false;
+    }
+
+    public String toString() {
+        return this.taskName;
+    }
+}
