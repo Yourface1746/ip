d73214110bd679d2af504285d83ab73a1d6f46f9
Yourface1746 <Zshouhe@gmail.com>
2025-08-26 09:40:12 +0800

    Level-8: Added Date and Time

--- src/main/java/sunday/Event.java
+++ src/main/java/sunday/Event.java
@@ -1,23 +1,45 @@
 package sunday;
 
-public class Event extends Task {
-    private String startDate;
-    private String endDate;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
-    public Event(String taskName, String startDate, String endDate, boolean done) {
+public class Event extends Task {
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    public Event(String taskName, LocalDateTime startDate, LocalDateTime endDate, boolean done) {
         super(taskName, done);
         this.startDate = startDate;
         this.endDate = endDate;
     }
 
+    public Event(String taskName, String startDate, String endDate, boolean done) {
+        super(taskName, done);
+        this.startDate = DateTimeHelper.parseDateTime(startDate);
+        this.endDate = DateTimeHelper.parseDateTime(endDate);
+    }
+
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(from: " + this.startDate + "to: " + this.endDate + ")";
+        return "[E]"
+                + super.toString()
+                + "(from: "
+                + this.startDate.format(DateTimeHelper.dateTimePrint)
+                + " to: "
+                + this.endDate.format(DateTimeHelper.dateTimePrint)
+                + ")";
     }
 
     @Override
     public String convertor() {
-        return "E | " + (isDone() ? 1 : 0) + " | " + getTaskName()
-                + " | " + startDate + " | " + endDate;
+        return "E | "
+                + (isDone() ? 1 : 0)
+                + " | "
+                + getTaskName()
+                + " | "
+                + this.startDate.format(DateTimeHelper.dateTimeSave)
+                + " | "
+                + this.endDate.format(DateTimeHelper.dateTimePrint);
     }
 }
