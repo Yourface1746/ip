9a2c65f4f53b63d6e3c4f82e6a1ad355c92770f2
Yourface1746 <Zshouhe@gmail.com>
2025-09-18 09:54:37 +0800

    A-MoreTesting: add TaskList tests with fake storage git commit --amend --no-edit

--- src/test/java/sunday/ParserTest.java
+++ src/test/java/sunday/ParserTest.java
@@ -9,13 +9,14 @@ import task.Todo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParserTest {
     @Test
     public void parseToDoLine() {
         Task t = Parser.parseTaskLine("T | 1 | read book");
-        assertTrue(t instanceof Todo);
+        assertInstanceOf(Todo.class, t);
         assertTrue(t.isDone());
         assertEquals("T | 1 | read book", t.convertor());
     }
@@ -23,7 +24,7 @@ public class ParserTest {
     @Test
     public void parseDeadline() {
         Task t = Parser.parseTaskLine("D | 0 | return book | 2025-08-25");
-        assertTrue(t instanceof Deadline);
+        assertInstanceOf(Deadline.class, t);
         assertFalse(t.isDone());
         assertEquals("D | 0 | return book | 2025-08-25", t.convertor());
         assertTrue(t.toString().toLowerCase().contains("by"));
@@ -32,7 +33,7 @@ public class ParserTest {
     @Test
     public void parseEventline() {
         Task t = Parser.parseTaskLine("E | 0 | meeting | 2025-08-25 1800 | 2025-08-26 1700");
-        assertTrue(t instanceof Event);
+        assertInstanceOf(Event.class, t);
         assertFalse(t.isDone());
         assertEquals("E | 0 | meeting | 2025-08-25 1800 | 2025-08-26 1700", t.convertor());
         assertTrue(t.toString().toLowerCase().contains("from"));
